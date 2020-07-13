2d06c6549a7640df52b9bfadb4c37832f1cae80c
Diego Andres Porro Vargas <diegoporro25@gmail.com>
2020-07-12 22:46:02 -0400

    Update settings.py
    Modifing ALLOWED_HOSTS

--- Migtel_Admin/settings.py
+++ Migtel_Admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=4je-$$f!jmut7k_3rr=%h^&%3i*%nfih382ug31-=ohq+!2t!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['migtel-web.herokuapp.com']
 
 
 # Application definition
